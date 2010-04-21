f21556fc59b0f8ab5222ddc13d92c40d9bb150b0
Daniel Plaisted <dsplaisted@gmail.com>
2010-04-21 07:19:02 -0700

    TWS plugin will ignore ticks with negative prices, except for futures and options (since the negative price could be valid for these)

--- TWSPlugin/TWSPlugin.cs
+++ TWSPlugin/TWSPlugin.cs
@@ -173,7 +173,10 @@ namespace RightEdge.TWSCSharpPlugin
 				data.time = GetAccountTime("price tick");
 				data.price = (double)e.Price;
 
-				if (data.price <= 0)
+				if (data.price <= 0 &&
+					symbol.AssetClass != AssetClass.Future &&
+					symbol.AssetClass != AssetClass.Option &&
+					symbol.AssetClass != AssetClass.FuturesOption)
 				{
 					//	GBP/USD was getting bid and ask ticks with a price of zero, so ignore these.
 					//	4/20/2010 - A user reported various forex symbols were getting negative prices, so these will also be ignored
