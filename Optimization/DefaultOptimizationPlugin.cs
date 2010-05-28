6c4950723595b6e9767f510487404716b427f461
Daniel Plaisted <dsplaisted@gmail.com>
2010-05-27 23:31:03 -0700

    Adding source code for default optimization plugin and hill climbing optimization.

---
+++ Optimization/DefaultOptimizationPlugin.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using RightEdge.Common;
+using System.Globalization;
+using System.IO;
+
+namespace RightEdge.Optimization
+{
+	[DisplayName("Default (Brute Force) Optimization Plugin")]
+	public class DefaultOptimizationPlugin : OptimizationPlugin
+	{
+		protected override bool ShowOptimizationSettings(SystemRunSettings runSettings, System.Windows.Forms.IWin32Window owner)
+		{
+			return base.ShowOptimizationSettings(runSettings, owner);
+		}
+
+		protected override void LoadOptimizationSettingsFromFile(SystemRunSettings runSettings, string filename)
+		{
+			base.LoadOptimizationSettingsFromFile(runSettings, filename);
+		}
+
+		public override List<OptimizationResult> RunOptimization(SystemRunSettings runSettings)
+		{
+			var optimizationResults = new List<OptimizationResult>();
+			int[] optProgress = new int[OptimizationParameters.Count];
+			Dictionary<string, double> paramDict = new Dictionary<string, double>(OptimizationParameters.Count);
+			int runNo = 0;
+			while (true)
+			{
+				runNo++;
+
+				int totalRuns = 1;
+
+				for (int i = 0; i < OptimizationParameters.Count; i++)
+				{
+					SystemParameterInfo param = OptimizationParameters[i];
+					totalRuns *= param.NumSteps;
+					double currentValue;
+					if (param.NumSteps > 1)
+					{
+						double stepSize = (double)((Decimal)(param.High - param.Low) / (Decimal)(param.NumSteps - 1));
+						currentValue = param.Low + optProgress[i] * stepSize;
+					}
+					else
+					{
+						currentValue = param.Low;
+					}
+					paramDict[param.Name] = currentValue;
+				}
+
+				string overallProgressText = string.Format("Optimization run {0} of {1}", runNo, totalRuns);
+
+				double overallProgress = ((double)runNo - 1) / totalRuns;
+				UpdateProgress(overallProgressText, overallProgress, "Initializing...", 0);
+
+				runSettings.SystemParameters = paramDict.ToList();
+				OptimizationResult result = RunSystem(runSettings, (currentItem, totalItems, currentTime) =>
+				{
+					double currentRunProgress = (double)currentItem / totalItems;
+					string currentRunProgressText = null;
+					if (currentTime != DateTime.MinValue)
+					{
+						CultureInfo culture = BarUtils.GetCurrencyCulture(runSettings.AccountCurrency);
+						currentRunProgressText = "Current Run Progress: " + currentTime.ToString(culture.DateTimeFormat);
+					}
+					UpdateProgress(null, overallProgress + (currentRunProgress / totalRuns), currentRunProgressText, currentRunProgress);
+				});
+
+				if (!runSettings.SaveOptimizationResults)
+				{
+					File.Delete(result.ResultsFile);
+				}
+
+				optimizationResults.Add(result);
+
+				bool done = true;
+				for (int i = 0; i < optProgress.Length; i++)
+				{
+					optProgress[i]++;
+					if (optProgress[i] < OptimizationParameters[i].NumSteps)
+					{
+						done = false;
+						break;
+					}
+					else
+					{
+						optProgress[i] = 0;
+					}
+				}
+
+				if (done)
+				{
+					break;
+				}
+			}
+
+			//	Remove parameters that weren't optimized from the list so that they don't show up as columns in the optimization results
+			HashSet<string> unoptimizedParameters = new HashSet<string>(OptimizationParameters.Where(p => p.NumSteps <= 1).Select(p => p.Name));
+			foreach (var result in optimizationResults)
+			{
+				result.ParameterValues = result.ParameterValues.Where(p => !unoptimizedParameters.Contains(p.Key)).ToList();
+			}
+
+
+			return optimizationResults;
+		}
+	}
+}
---
+++ Optimization/HillClimbingOptimizationPlugin.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RightEdge.Common;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+
+namespace RightEdge.Optimization
+{
+	[DisplayName("Hill Climbing Optimization Plugin")]
+#if !DEBUG
+	[ExcludedPlugin]
+#endif
+	public class HillClimbingOptimizationPlugin : OptimizationPlugin
+	{
+		public int MaxRuns { get; set; }
+
+		private Random _rnd = new Random();
+
+		List<SystemParameterInfo> _parametersToOptimize;
+		List<SystemParameterInfo> _unOptimizedParameters;
+
+		public HillClimbingOptimizationPlugin()
+		{
+			MaxRuns = 25;
+		}
+
+		void SetupOptimization()
+		{
+			_parametersToOptimize = OptimizationParameters.Where(p => p.NumSteps > 1).ToList();
+			_unOptimizedParameters = OptimizationParameters.Where(p => p.NumSteps <= 1).ToList();
+		}
+
+		protected override bool ShowOptimizationSettings(SystemRunSettings runSettings, System.Windows.Forms.IWin32Window owner)
+		{
+			//	Don't pop up a dialog, just start the optimization
+			return true;
+			//return base.ShowOptimizationSettings(runSettings, owner);
+		}
+
+		public override List<OptimizationResult> RunOptimization(SystemRunSettings runSettings)
+		{
+			SetupOptimization();
+
+			List<OptimizationResult> ret = new List<OptimizationResult>();
+
+			double overallProgress = 0;
+			int runNo = 0;
+
+			SystemProgressUpdate progressCallback = (currentItem, totalItems, currentTime) =>
+			{
+				double currentRunProgress = (double)currentItem / totalItems;
+				string currentRunProgressText = null;
+				if (currentTime != DateTime.MinValue)
+				{
+					CultureInfo culture = BarUtils.GetCurrencyCulture(runSettings.AccountCurrency);
+					currentRunProgressText = "Current Run Progress: " + currentTime.ToString(culture.DateTimeFormat);
+				}
+				UpdateProgress(null, overallProgress + (currentRunProgress / MaxRuns), currentRunProgressText, currentRunProgress);
+			};
+
+			//	Get baseline results
+			//runNo++;
+			//OptimizationResult currentBest = RunSystem(runSettings, progressCallback);
+			//ret.Add(currentBest);
+			
+			OptimizationResult currentBest = null;
+
+			double[] currentParameters = _parametersToOptimize.Select(p => p.Value).ToArray();
+
+			while (runNo < MaxRuns)
+			{
+				runNo++;
+
+				string overallProgressText = string.Format("Optimization run {0} of {1}", runNo, MaxRuns);
+				overallProgress = ((double)runNo - 1) / MaxRuns;
+				UpdateProgress(overallProgressText, overallProgress, "Initializing...", 0);
+
+				if (currentBest != null)
+				{
+					//	Choose parameters to use for next run
+					double[] newParameters = GetRandomNeighbor(currentParameters, 1.0);
+					runSettings.SystemParameters = new List<KeyValuePair<string, double>>();
+					for (int i = 0; i < _parametersToOptimize.Count; i++)
+					{
+						runSettings.SystemParameters.Add(new KeyValuePair<string, double>(_parametersToOptimize[i].Name, newParameters[i]));
+					}
+					runSettings.SystemParameters.AddRange(_unOptimizedParameters.Select(p => new KeyValuePair<string, double>(p.Name, p.Value)));
+				}
+
+				OptimizationResult newResults = RunSystem(runSettings, progressCallback);
+
+				if (currentBest == null ||
+					EvaluateResults(newResults) >= EvaluateResults(currentBest))
+				{
+					currentBest = newResults;
+					ret.Add(currentBest);
+				}
+				else
+				{
+					File.Delete(newResults.ResultsFile);
+				}
+			}
+
+			//	Remove parameters that weren't optimized from the list so that they don't show up as columns in the optimization results
+			HashSet<string> unoptimizedParameters = new HashSet<string>(OptimizationParameters.Where(p => p.NumSteps <= 1).Select(p => p.Name));
+			foreach (var result in ret)
+			{
+				result.ParameterValues = result.ParameterValues.Where(p => !unoptimizedParameters.Contains(p.Key)).ToList();
+			}
+
+			return ret;
+		}
+
+		//	This will be the value that the optimization process optimizes.
+		//	It will search for the set of parameters that gives the highest result for this number.
+		public virtual double EvaluateResults(OptimizationResult result)
+		{
+			//	By default, just use the APR.
+			return result.FinalStatistic.APR;
+		}
+
+		public virtual double[] GetRandomNeighbor(double[] current, double velocity)
+		{
+			//	Pick a random direction in n-dimensional space
+			//	http://mathworld.wolfram.com/HyperspherePointPicking.html
+			double[] vector = new double[_parametersToOptimize.Count].Select(d => GaussianRand()).ToArray();
+			double length = Math.Sqrt(vector.Select(d => d * d).Sum());
+			vector = vector.Select(d => d / length).ToArray();
+
+			List<KeyValuePair<string, double>> ret = new List<KeyValuePair<string, double>>();
+			for (int i = 0; i < _parametersToOptimize.Count; i++)
+			{
+				var parameter = _parametersToOptimize[i];
+				double stepSize = (parameter.High - parameter.Low) / parameter.NumSteps;
+				double newValue = current[i] + (stepSize * vector[i] * velocity);
+
+				if (newValue > parameter.High)
+				{
+					newValue = parameter.High;
+				}
+				if (newValue < parameter.Low)
+				{
+					newValue = parameter.Low;
+				}
+
+				vector[i] = newValue;
+			}
+
+			return vector;
+		}
+
+		//	http://stackoverflow.com/questions/218060/random-gaussian-variables
+		private double GaussianRand()
+		{
+			double u1 = _rnd.NextDouble(); //these are uniform(0,1) random doubles
+			double u2 = _rnd.NextDouble();
+			double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+						 Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+			//double randNormal =
+			//             mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+			return randStdNormal;
+		}
+	}
+}
